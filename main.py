bbfdbcb68423ba8f211a65269cf9608411541544
KwakYeonJu <wnmkwak@gmail.com>
2024-09-30 16:31:30 +0900

    제곱커밋

--- main.py
+++ main.py
@@ -21,12 +21,12 @@ print(num1, '-', num2, '=', res)
 
 ## 함수 선언부
 def mul_func(n1, n2):
-    return n1 * n2
+    return n1 ** n2
 ## 전역 변수부
 num1, num2, res = 100, 200, 0
 ## 메인 코드부
 res = mul_func(num1, num2)
-print(num1, '*', num2, '=', res)
+print(num1, '**', num2, '=', res)
 
 
 
